ab3a6fd8f1d139c2d2f766e65b2042e606ba54e5
kminjae121 <kbckmj2050@gmail.com>
2024-11-19 19:14:52 +0900

    feat- KeyTag

--- Assets/02.Scripts/KHY/OpenTheDoor.cs
+++ Assets/02.Scripts/KHY/OpenTheDoor.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class OpenTheDoor : MonoBehaviour
 {
+    private static int _keyNumber;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("LockKey1"))
+        if (collision.gameObject.CompareTag($"LockKey{_keyNumber}"))
         {
             gameObject.TryGetComponent(out Animator animator);
 
             animator.SetBool("Open", true);
+
+            _keyNumber++;
         }
         else
             return;
