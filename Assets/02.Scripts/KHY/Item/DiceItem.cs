06b713f7ebdc22619c5a4b441767fe35255dc9bb
fish2363 <a01022639410@gmail.com>
2024-11-22 14:56:06 +0900

    feat : Dice

---
+++ Assets/02.Scripts/KHY/Item/DiceItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceItem : MonoBehaviour, IUseItem
+{
+    public void Use()
+    {
+        print("¡÷ªÁ¿ß");
+    }
+}
